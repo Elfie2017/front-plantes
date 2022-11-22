61123157307070eda3e8030cca427fd4a1b54e10
Elfie2017 <floriane.coislier@sncf.fr>
2022-11-22 10:49:58 +0100

    git phil

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { list_products } from '../data';
-import SideBar from '../components/SideBar';
-import { useState } from 'react';
+import { list_products } from "../data";
+import SideBar from "../components/SideBar";
+import { useEffect, useState } from "react";
 
 export interface Plante {
   id: string;
@@ -15,16 +15,28 @@ export interface Plante {
 /**
  * Ici les constantes ou variables dont la modification de valeur ne provoquera pas directement de re-render
  */
-const listePlantes: Plante[] = list_products;
+let listePlantes: Plante[] = [];
 let checkedCateg: string[] = [];
 
 const Home = () => {
   const [listPlantDisplayed, setListPlantDisplayed] = useState<Plante[]>([
     ...listePlantes,
   ]);
-
+  useEffect(() => {
+    fetch("http://localhost:8080/api/plantes")
+      .then((reponse) => reponse.json())
+      .then((plantsData) => {
+        console.log(plantsData);
+        console.log(plantsData.data);
+        listePlantes = plantsData.data;
+        setListPlantDisplayed(plantsData.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   const handleCheckCategories = (mesCategoriesChecked: string[]) => {
-    console.log('categories checked', mesCategoriesChecked);
+    console.log("categories checked", mesCategoriesChecked);
     /**
      * Filtrer nos données ici
      */
@@ -43,18 +55,27 @@ const Home = () => {
   };
 
   return (
-    <div className='d-flex align-items-stretch'>
+    <div className="d-flex align-items-stretch">
       <SideBar
         listElementPlant={listePlantes}
         onChangeCategoriesCheck={handleCheckCategories}
       />
-      <div className='container-fluid custom-main'>
+      <div className="container-fluid custom-main d-flex flex-wrap">
         {listPlantDisplayed.map((plante, i) => (
-          <li key={i}>
-            {plante.name} - {plante.category} - 💵 {plante.unitprice_ati}€ - ⭐
-            {plante.rating}
-          </li>
-        ))}{' '}
+          <div key={plante.id} className="card m-3" style={{ width: "18rem" }}>
+            <img
+              src={`http://localhost:8080/assets/${plante.url_picture}`}
+              className="card-img-top"
+              alt="icone plante"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{plante.name}</h5>
+              <p className="card-text">{plante.category}</p>
+              <p className="card-text">{plante.unitprice_ati} €</p>
+              <p className="card-text">rating : {plante.rating}</p>
+            </div>
+          </div>
+        ))}{" "}
       </div>
     </div>
   );
